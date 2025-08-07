140dc9d9a02afa3c7c685f3e838975abcd4f8137
BaikNadia <baikowa.nadia@gmail.com>
2025-08-07 19:27:05 +0300

    changes views

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from dogs.models import Dog
+
+
+def dogs_list(request):
+    dogs = Dog.objects.all()
+    context = {"dogs": dogs}
+    return render(request, "dogs_list.html", context)
+
+
+def dogs_detail(request, pk):
+    dog = get_object_or_404(Dog, pk=pk)
+    context = {"dog": dog}
+    return render(request, 'dogs_detail.html', context)
