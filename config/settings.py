093a447bd382c1b4474f02b2a43ee1ed02b3f605
BaikNadia <baikowa.nadia@gmail.com>
2025-08-07 19:26:53 +0300

    changes setting

--- config/settings.py
+++ config/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Медиатека (Media)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
