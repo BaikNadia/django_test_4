b28fd9f5a9315e85826529613815545b1683be05
BaikNadia <baikowa.nadia@gmail.com>
2025-08-12 10:43:55 +0300

    change admin

---
+++ library/views.py
@@ -0,0 +1,32 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
+
+class BooksListView(ListView):
+    model = Book
+    template_name = 'library/books_list.html'
+    context_object_name = 'books'
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'publication_date', 'author']
+    template_name = 'library/book_form.html'
+    success_url = reverse_lazy('library:books_list')
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'library/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'publication_date', 'author']
+    template_name = 'library/book_form.html'
+    success_url = reverse_lazy('library:books_list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'library/book_confirm_delete.html'
+    success_url = reverse_lazy('library:books_list')
