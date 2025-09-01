699bf67dc216e8404c1bf4e00ddb3ff1afd6aa84
BaikNadia <baikowa.nadia@gmail.com>
2025-09-01 18:00:13 +0300

    change config/

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dogs/', include('dogs.urls', namespace='dogs')),
-    path('', include('library.urls', namespace='library')),
-
+    path('books/', include('library.urls', namespace='library')),
+    path('users/', include('users.urls', namespace='users')),
+    path('', RedirectView.as_view(url='books/', permanent=True), name='home'),
 ]
 
 if settings.DEBUG:
